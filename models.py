6bd424107cd0b8b32293b5605bd3f5eff8152094
Alexander Botello <alexander.botello@g.austincc.edu>
2017-03-10 11:21:39 -0600

    Remove db url

--- models.py
+++ models.py
@@ -10,10 +10,7 @@ def db_connect():
     Performs database connection
     Returns sqlalchemy engine instance
     """
-    return create_engine('postgres://fcvxvbdsuotypy:a3b010cca1fa4e6949ff39c11c6'
-                         'e0b9edf9ce67a650535436dc349ba29b8c751@ec2-54-243-253-'
-                         '17.compute-1.amazonaws.com:5432/'
-                         'dfl66jjoa0etqc', echo=False)
+    return create_engine('DB_URL_HERE', echo=False)
 
 
 def create_battletag_table(engine):
